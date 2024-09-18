d308a365daace5f4f69643a6f090230ae969e7e6
Rafaela Vieira Cabral <rafaelavieiracabral04@gmail.com>
2024-09-17 21:50:03 -0300

    feat: general questions answering chat bot with prompt input

--- src/llm/google_llm.py
+++ src/llm/google_llm.py
@@ -1,11 +1,13 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
 def create_llm():
     return ChatGoogleGenerativeAI(
         model="gemini-1.5-pro",
+        api_key=os.getenv("GEMINI_API_KEY"),  
         temperature=0,
         max_tokens=None,
         timeout=None,
--- src/main.py
+++ src/main.py
@@ -3,16 +3,26 @@ from llm.google_llm import create_llm
 def main():
     llm = create_llm()
 
-    messages = [
-        (
-            "system",
-            "Você é um assistente que realiza traduções do inglês para o português. Traduza a sentença do usuário.",
-        ),
-        ("human", "I love programming."),
-    ]
+    print("Bem-vindo! Pergunte o que quiser (ou digite 'exit' para sair).")
 
-    ai_msg = llm.invoke(messages)
-    print(ai_msg.content)
+    while True:
+        user_input = input("Você: ")
+
+        if user_input.lower() == "exit":
+            print("Encerrando o chatbot. Até logo!")
+            break
+
+        messages = [
+            (
+                "system",
+                "Você é um assistente que responde perguntas de qualquer tipo.",
+            ),
+            ("human", user_input),
+        ]
+
+        ai_msg = llm.invoke(messages)
+
+        print(f"Assistente: {ai_msg.content}")
 
 if __name__ == "__main__":
     main()
